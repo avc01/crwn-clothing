14b10e2b88b6335ff596beab33149c979cd9a921
Andres Viquez Chavarria <andresvvcc@gmail.com>
2022-01-31 20:20:03 -0600

    redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 import "./header.styles.scss";
 
 const Header = ({ currentUser }) => {
@@ -31,4 +32,10 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.user.currentUser,
+  };
+};
+
+export default connect(mapStateToProps)(Header);
